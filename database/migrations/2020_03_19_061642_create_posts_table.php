52f2bd2c4d11afb6b64476aa7339fe70b7724cab
Fuad <fuadm03@gmail.com>
2020-03-19 13:24:09 +0700

    edit post migration

--- database/migrations/2020_03_19_061642_create_posts_table.php
+++ database/migrations/2020_03_19_061642_create_posts_table.php
@@ -14,8 +14,15 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('caption');
+            $table->string('image');
+
             $table->timestamps();
+
+            $table->index('user_id');
+            
         });
     }
 
