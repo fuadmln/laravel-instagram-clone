53d438da1416605ae015189aa25e840372cc8d2d
Fuad <fuadm03@gmail.com>
2020-03-19 11:30:40 +0700

    edit username to username from db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img src="/img/insta.png" alt="profile img" class="rounded-circle" style="height: 200px">
         </div>
         <div class="col-9 pt-5"> <!-- profile info -->
-            <div><h1>username</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-4"><strong>154</strong> post</div>
                 <div class="pr-4"><strong>22k</strong> followers</div>
@@ -21,14 +21,15 @@
 
     <div class="row pt-5"> <!-- post -->
         <div class="col-4">
-            <img src="img/post1.png" alt="" class="w-100">
+            <img src="/img/post1.png" alt="" class="w-100">
         </div>
         <div class="col-4">
-            <img src="img/post2.png" alt="" class="w-100">
+            <img src="/img/post2.png" alt="" class="w-100">
         </div>
         <div class="col-4">
-            <img src="img/post3.png" alt="" class="w-100">
+            <img src="/img/post3.png" alt="" class="w-100">
         </div>
     </div>
+
 </div>
 @endsection
