f0868ec36d743e5da2d31232744a25c110071bb4
Fuad <fuadm03@gmail.com>
2020-03-19 08:21:09 +0700

    setting profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5"> <!-- profile image -->
+            <img src="/img/insta.png" alt="profile img" class="rounded-circle" style="height: 200px">
+        </div>
+        <div class="col-9 pt-5"> <!-- profile info -->
+            <div><h1>username</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>154</strong> post</div>
+                <div class="pr-4"><strong>22k</strong> followers</div>
+                <div class="pr-4"><strong>20</strong> following</div>
             </div>
         </div>
     </div>
