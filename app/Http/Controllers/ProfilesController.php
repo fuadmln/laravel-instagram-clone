d1c02ede54791eb44e7b893ba4e69621c2151214
Fuad <fuadm03@gmail.com>
2020-03-19 13:53:32 +0700

    move home.blade to profile n rename to index, set ctrl

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,7 +10,7 @@ class ProfilesController extends Controller
     public function index($user)
     {
     	$user = User::findOrFail($user);
-        return view('home', [
+        return view('profile.index', [
         	'user'=>$user]);
     }
 }
