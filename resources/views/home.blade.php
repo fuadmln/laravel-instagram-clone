f7851846fb0b2998c73aa89ba3380c17d8824e16
Fuad <fuadm03@gmail.com>
2020-03-19 08:49:54 +0700

    seting post ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row"> <!-- profile -->
         <div class="col-3 p-5"> <!-- profile image -->
             <img src="/img/insta.png" alt="profile img" class="rounded-circle" style="height: 200px">
         </div>
@@ -13,6 +13,21 @@
                 <div class="pr-4"><strong>22k</strong> followers</div>
                 <div class="pr-4"><strong>20</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">instagram.com</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, ad, commodi? Quae fuga, iste recusandae incidunt dolore possimus enim odit, eaque ullam perspiciatis, ducimus esse vitae sunt quos omnis voluptatem.</div>
+            <div><a href="#">instagram.com/username</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5"> <!-- post -->
+        <div class="col-4">
+            <img src="img/post1.png" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="img/post2.png" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="img/post3.png" alt="" class="w-100">
         </div>
     </div>
 </div>
